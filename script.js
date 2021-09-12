df3446805c4560145d2eff174a4b7b95dd52c19c
Anton <antonbogatirev1@gmail.com>
2021-09-11 17:59:56 -0700

    Displaying Balance, Income & Expenses
    Added a updateValues function that calculates and sets the Balance, Income and Expenses.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
 const list = document.getElementById('list');
 
 const dummyTransactions = [
@@ -26,13 +29,34 @@ function addTransactionDOM(transaction) {
     `;
 
     list.appendChild(item);
-}
+};
+
+// Update the balance, income and expense 
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const total = amounts.reduce((acc, item) => (acc + item), 0).toFixed(2);
+
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+    const expense = (amounts
+        .filter(item => item < 0)
+        .reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
+
+    balance.innerText = `$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
+};
 
 // Init app
 function init() {
     list.innerHTML = '';
 
     transactions.forEach(addTransactionDOM);
-}
+    updateValues();
+};
 
 init();
