18425d647e7158a94d8244ee671ddbc27ad1fc81
Anton <antonbogatirev1@gmail.com>
2021-09-12 09:26:20 -0700

    Submit form & add transaction
    Added a addTransaction function that runs when user submits the form.

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const balance = document.getElementById('balance');
 const money_plus = document.getElementById('money-plus');
 const money_minus = document.getElementById('money-minus');
 const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
 
 const dummyTransactions = [
     { id: 1, text: 'Flower', amount: -20 },
@@ -11,6 +14,35 @@ const dummyTransactions = [
 ];
 
 let transactions = dummyTransactions;
+
+// Add transaction
+function addTransaction(e) {
+    e.preventDefault();
+
+    if(text.value.trim() === '' || amount.value.trim() === '') {
+        alert('Please add a text and amount');
+    } else {
+        const transaction = {
+            id: generateID(),
+            text: text.value,
+            amount: +amount.value
+        };
+        
+        transactions.push(transaction);
+
+        addTransactionDOM(transaction);
+
+        updateValues();
+
+        text.value = '';
+        amount.value = '';
+    };
+};
+
+//Generate random ID
+function generateID() {
+    return Math.floor(Math.random() * 100000000);
+};
 
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
@@ -60,3 +92,5 @@ function init() {
 };
 
 init();
+
+form.addEventListener('submit', addTransaction);
